df8d060559631f379393b0893a6b284cbe5debc6
David T <79376703+davidTens@users.noreply.github.com>
2023-05-21 23:15:18 +0400

    Initial Commit

---
+++ TestProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProj
+//
+//  Created by Daviti Tsveniashvili on 5/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
