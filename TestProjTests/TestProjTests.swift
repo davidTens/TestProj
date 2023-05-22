0dbf387e18bbdf188b4a2dec0cd7dc1c87f91556
David T <79376703+davidTens@users.noreply.github.com>
2023-05-22 23:22:32 +0400

    tests

--- TestProjTests/TestProjTests.swift
+++ TestProjTests/TestProjTests.swift
@@ -3,9 +3,7 @@ import XCTest
 
 final class TestProjTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+
         let testNumber = 9
         let sum = 81
         XCTAssertEqual(testNumber * testNumber, sum)
