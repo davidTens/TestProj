13ed8c3bff5a3ce5c71259259aa087ca31386e5c
David T <79376703+davidTens@users.noreply.github.com>
2023-05-23 00:18:05 +0400

    service tests

--- TestProjTests/TestProjTests.swift
+++ TestProjTests/TestProjTests.swift
@@ -2,10 +2,17 @@ import XCTest
 @testable import TestProj
 
 final class MockItemsViewService: XCTestCase, ItemsServiceProviding {
+
+    var shouldSuccess = true
     
     func getData(itemsPerPage: Int, completion: @escaping TestProj.APIResult) {
         let itemModel = ArtObjects(id: "23", title: "title", longTitle: "longTitle", principalOrFirstMaker: "maker", productionPlaces: ["Amsterdam", "Vienna"], headerImage: .init(url: ""))
-        completion(.success([itemModel]))
+
+        if shouldSuccess {
+            completion(.success([itemModel]))
+        } else {
+            completion(.failure(.invalidURL))
+        }
     }
 }
 
@@ -22,7 +29,45 @@ final class TestProjTests: XCTestCase {
     }
 
     override func tearDown() {
+        viewService = nil
+        viewModel = nil
         super.tearDown()
+    }
+
+    func testShuoldSuccess() {
+        viewService.shouldSuccess = true
+
+        let exptectation = XCTestExpectation(description: "API request should succeed")
+
+        viewService.getData(itemsPerPage: 10) { result in
+            switch result {
+            case .success(let items):
+                XCTAssertNotNil(items)
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+            exptectation.fulfill()
+        }
+
+        wait(for: [exptectation], timeout: 4)
+        XCTAssertNotNil(viewModel.artObjectModel)
+    }
+
+    func testShuoldFail() {
+        viewService.shouldSuccess = false
+
+        let expectation = XCTestExpectation(description: "API request should fail")
+
+        viewService.getData(itemsPerPage: 10) { result in
+            switch result {
+            case .success(_):
+                XCTFail("Success, however should fail")
+            case .failure(let error):
+                XCTAssertNotNil(error)
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 4)
     }
 
     func testExample() throws {
