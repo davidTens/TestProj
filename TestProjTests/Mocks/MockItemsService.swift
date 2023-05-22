2886dfea8e2f6a72c6d1e6be6f83a27e827444b6
David T <79376703+davidTens@users.noreply.github.com>
2023-05-23 00:20:10 +0400

    new mocks file

---
+++ TestProjTests/Mocks/MockItemsService.swift
@@ -0,0 +1,17 @@
+import XCTest
+@testable import TestProj
+
+final class MockItemsViewService: XCTestCase, ItemsServiceProviding {
+
+    var shouldSuccess = true
+    
+    func getData(itemsPerPage: Int, completion: @escaping TestProj.APIResult) {
+        let itemModel = ArtObjects(id: UUID().uuidString, title: "title", longTitle: "longTitle", principalOrFirstMaker: "maker", productionPlaces: ["Amsterdam", "Vienna"], headerImage: .init(url: ""))
+
+        if shouldSuccess {
+            completion(.success([itemModel]))
+        } else {
+            completion(.failure(.invalidURL))
+        }
+    }
+}
--- TestProjTests/TestProjTests.swift
+++ TestProjTests/TestProjTests.swift
@@ -1,21 +1,6 @@
 import XCTest
 @testable import TestProj
 
-final class MockItemsViewService: XCTestCase, ItemsServiceProviding {
-
-    var shouldSuccess = true
-    
-    func getData(itemsPerPage: Int, completion: @escaping TestProj.APIResult) {
-        let itemModel = ArtObjects(id: "23", title: "title", longTitle: "longTitle", principalOrFirstMaker: "maker", productionPlaces: ["Amsterdam", "Vienna"], headerImage: .init(url: ""))
-
-        if shouldSuccess {
-            completion(.success([itemModel]))
-        } else {
-            completion(.failure(.invalidURL))
-        }
-    }
-}
-
 final class TestProjTests: XCTestCase {
 
     private var viewModel: ItemsViewModel!
