a4c88e87294b028a9a215814c6008be7ee5d742d
David T <79376703+davidTens@users.noreply.github.com>
2023-05-25 18:01:11 +0400

    code improvements

--- TestProj/Sources/Features/Home/ItemsFactory.swift
+++ TestProj/Sources/Features/Home/ItemsFactory.swift
@@ -2,12 +2,17 @@ import Foundation
 
 struct ItemsFactory {
     static func makeViewModel() -> ItemsViewModel {
+        let client = makeNetworkClient()
+        let viewService = ItemsViewService(networkClient: client)
+        let viewModel = ItemsViewModel(viewService: viewService)
+        return viewModel
+    }
+
+    private static func makeNetworkClient() -> NetworkClient {
         let urlRepo = URLRepository()
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
         let client = NetworkClient(urlRepository: urlRepo, decoder: decoder)
-        let viewService = ItemsViewService(networkClient: client)
-        let viewModel = ItemsViewModel(viewService: viewService)
-        return viewModel
+        return client
     }
 }
